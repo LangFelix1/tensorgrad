Add unbroadcast for correct backpropagation in case of broadcasting

src/tensor.py
@@ -15,12 +15,12 @@ class Tensor:
         self._op = _op
 
     def __add__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other)
+        other = Tensor._ensure_tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
+            self.grad += Tensor.unbroadcast(out.grad, self.data.shape)
+            other.grad += Tensor.unbroadcast(out.grad, other.data.shape)
         out._backward = _backward
 
         return out
@@ -29,40 +29,50 @@ class Tensor:
         return self + (-other)
     
     def __mul__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other)
+        other = Tensor._ensure_tensor(other)
         out = Tensor(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
+            self.grad += Tensor.unbroadcast(other.data * out.grad, self.data.shape)
+            other.grad += Tensor.unbroadcast(self.data * out.grad, other.data.shape)
         out._backward = _backward
+
+        return out
     
     def __truediv__(self, other):
         return self * other**-1
 
     def __pow__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other)
+        other = Tensor._ensure_tensor(other)
         out = Tensor(self.data ** other.data, (self, other), f'**{other}')
 
         def _backward():
-            self.grad += (other.data * self.data**(other.data - 1)) * out.grad
-            other.grad += (out.data * cp.log(self.data)) * out.grad
+            self.grad += Tensor.unbroadcast((other.data * self.data**(other.data - 1)) * out.grad, self.data.shape)
+            other.grad += Tensor.unbroadcast((out.data * cp.log(self.data)) * out.grad, other.data.shape)
         out._backward = _backward
+
+        return out
     
     def __matmul__(self, other):
-        other = other if isinstance(other, Tensor) else Tensor(other)        
+        other = Tensor._ensure_tensor(other)       
         out_data = cp.matmul(self.data, other.data)
         out = Tensor(out_data, (self, other), '@')
 
         def _backward():
-            self.grad += cp.matmul(out.grad, other.data.T)
-            other.grad += cp.matmul(self.data.T, out.grad)
+            self.grad += Tensor.unbroadcast(cp.matmul(out.grad, other.data.T), self.data.shape)
+            other.grad += Tensor.unbroadcast(cp.matmul(self.data.T, out.grad), other.data.shape)
         out._backward = _backward
 
         return out
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __rmul__(self, other):
+        return self * other
 
-    def sum(self):
-        out_data = cp.sum(self.data)
+    def sum(self, dim=None, keepdim=False):
+        out_data = cp.sum(self.data, axis=dim, keepdims=keepdim)
         out = Tensor(out_data, _prev=(self,), _op='sum')
 
         def _backward():
@@ -89,9 +99,19 @@ class Tensor:
 
         for t in reversed(topo):
             t._backward()
-
-    def __neg__(self):
-        return self * -1
     
-    def __rmul__(self, other):
-        return self * other
+    @staticmethod
+    def unbroadcast(x, target_shape):
+        """
+        Reduces shape of x back to target_shape by summing over broadcasted dimensions
+        """
+        while x.ndim > len(target_shape):
+            x = x.sum(axis=0)
+        for i, (g, t) in enumerate(zip(x.shape, target_shape)):
+            if g != t:
+                x = x.sum(axis=i, keepdims=True)
+        return x.reshape(target_shape)
+    
+    @staticmethod
+    def _ensure_tensor(x):
+        return x if isinstance(x, Tensor) else Tensor(x)