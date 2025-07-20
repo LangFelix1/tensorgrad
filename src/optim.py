08563dccdd434b4ece06ac5b9760bb0a266a17a4
Felix <felix-lang@live.at>
2025-07-20 13:39:31 +0200

    Add AdamW optimizer

--- src/optim.py
+++ src/optim.py
@@ -1,3 +1,5 @@
+import cupy as cp
+
 class Optimizer:
     def __init__(self, params):
         self.params = list(params)
@@ -64,11 +66,11 @@ class Adam(Optimizer):
             if p not in self.state:
                 self.state[p] = {
                     "step": 0,
-                    "exp_avg": np.zeros_like(p.data),
-                    "exp_avg_sq": np.zeros_like(p.data),
+                    "exp_avg": cp.zeros_like(p.data),
+                    "exp_avg_sq": cp.zeros_like(p.data),
                 }
                 if self.amsgrad:
-                    self.state[p]["max_exp_avg_sq"] = np.zeros_like(p.data)
+                    self.state[p]["max_exp_avg_sq"] = cp.zeros_like(p.data)
 
             state = self.state[p]
             d_p = p.grad.copy()
@@ -91,9 +93,60 @@ class Adam(Optimizer):
             exp_avg_hat = exp_avg / bias_correction1
             if self.amsgrad:
                 max_exp_avg_sq = state['max_exp_avg_sq']
-                np.maximum(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
-                denom = np.sqrt(max_exp_avg_sq / bias_correction2) + self.eps
+                cp.maximum(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
+                denom = cp.sqrt(max_exp_avg_sq / bias_correction2) + self.eps
             else:
-                denom = np.sqrt(exp_avg_sq / bias_correction2) + self.eps
+                denom = cp.sqrt(exp_avg_sq / bias_correction2) + self.eps
+
+            p.data -= self.lr * exp_avg_hat / denom
+
+class AdamW(Optimizer):
+    def __init__(self, params, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, amsgrad=False):
+        super().__init__(params)
+        self.lr = lr
+        self.betas = betas
+        self.eps = eps
+        self.weight_decay = weight_decay
+        self.amsgrad = amsgrad
+        self.state = {}
+
+    def step(self):
+        for p in self.params:
+            if p.grad is None:
+                continue
+
+            if p not in self.state:
+                self.state[p] = {
+                    "step": 0,
+                    "exp_avg": cp.zeros_like(p.data),
+                    "exp_avg_sq": cp.zeros_like(p.data),
+                }
+                if self.amsgrad:
+                    self.state[p]["max_exp_avg_sq"] = cp.zeros_like(p.data)
+
+            state = self.state[p]
+            d_p = p.grad.copy()
+
+            p.data *= (1 - self.lr * self.weight_decay)
+
+            exp_avg = state["exp_avg"]
+            exp_avg_sq = state["exp_avg_sq"]
+            beta1, beta2 = self.betas
+
+            state["step"] += 1
+            step = state["step"]
+
+            exp_avg[:] = beta1 * exp_avg + (1 - beta1) * d_p            # exponential moving average of the gradient (m_t)
+            exp_avg_sq[:] = beta2 * exp_avg_sq + (1 - beta2) * d_p**2   # exponential moving average of the squared gradient (v_t)
+            bias_correction1 = 1 - beta1 ** step
+            bias_correction2 = 1 - beta2 ** step
+
+            exp_avg_hat = exp_avg / bias_correction1
+            if self.amsgrad:
+                max_exp_avg_sq = state['max_exp_avg_sq']
+                cp.maximum(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
+                denom = cp.sqrt(max_exp_avg_sq / bias_correction2) + self.eps
+            else:
+                denom = cp.sqrt(exp_avg_sq / bias_correction2) + self.eps
 
             p.data -= self.lr * exp_avg_hat / denom
